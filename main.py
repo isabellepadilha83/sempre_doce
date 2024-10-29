8e75685f3b8e5225ad42c032518c2f521dfd357c
isa <bellapadilha83@gmail.com>
2024-10-29 14:19:24 -0300

    todas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request, redirect,url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def inicial():
     return render_template("index.html")
 
