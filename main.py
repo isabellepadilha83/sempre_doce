d7621c47bb85b319a193a899b01bb69d99fe8809
isa <bellapadilha83@gmail.com>
2024-10-29 13:29:25 -0300

    todas

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template,request, redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicial():
+    return render_template("index.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/login", methods=["get", "post"])
+def login():
+    if request.method == "get":
+        return render_template("login.html")
+        elif request.method == "post":
+            usuario = request.form.get("username")
+            senha= request.form.get("password")
+
+            if username == "usuario1":
+                if password == "senhaUsuario":
+                    return redirect(url_for('upload'))
+
+@app.route("/upload")
+def upload ():
+    return render_template("upload.html")    
+        
+    
+if __name__ == '__main__':
+    app.run(debug=True)
