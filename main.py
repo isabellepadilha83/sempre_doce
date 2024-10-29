4d7ca40dbe1d6a209207053c18c4ccdf089bf5f2
isa <bellapadilha83@gmail.com>
2024-10-29 14:33:19 -0300

    Atualizando

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request, redirect,url_for
 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def inicial():
     return render_template("index.html")
 
